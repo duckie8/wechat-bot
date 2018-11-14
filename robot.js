73b6e29c9c069074c5152aaef824029924133f5a
吕继锋 <871593867@qq.com>
2018-11-14 22:14:39 +0800

    测试成功

--- robot.js
+++ robot.js
@@ -1,7 +1,27 @@
-const { Wechaty } = require('wechaty')
+const {
+  Wechaty
+} = require('wechaty')
 
-Wechaty.instance()
-.on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
-.on('login',  user => console.log(`User ${user.name()} logined`))
-.on('message',  message => console.log(`Message: ${message.content()}`))
-.init()
+const bot = new Wechaty({
+  name: "张玲亚丫丫丫丫！"
+});
+
+bot.on('scan', (url, code) => {
+  if (!/201|200/.test(String(code))) {
+    let loginUrl = url.replace(/\/qrcode\//, '/l/')
+    require('qrcode-terminal').generate(loginUrl,{small:true})
+  }
+  console.log(`${url}\n[${code}] Scan QR Code in above url to login: `)
+})
+bot.on('login', user => console.log(`User ${user} logined`))
+bot.on('message',send)
+bot.start()
+
+async function send(msg){
+  console.log(msg)
+  if(msg.payload.text==='发送'){
+    const helperContactA = await bot.Contact.find({ name: 'v.' })
+    await helperContactA.say('张玲亚真可爱啊~~~')
+  }
+  
+}
