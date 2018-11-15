6d94dd9e9c1043cfda3d37b50c00bfa3450d8905
Lvjf <871593867@qq.com>
2018-11-15 17:38:59 +0800

    接入图灵机器人

--- robot.js
+++ robot.js
@@ -1,6 +1,7 @@
 const {
   Wechaty
-} = require('wechaty')
+} = require('wechaty');
+const axios = require('axios');
 
 const bot = new Wechaty({
   name: "张玲亚丫丫丫丫！"
@@ -9,19 +10,51 @@ const bot = new Wechaty({
 bot.on('scan', (url, code) => {
   if (!/201|200/.test(String(code))) {
     let loginUrl = url.replace(/\/qrcode\//, '/l/')
-    require('qrcode-terminal').generate(loginUrl,{small:true})
+    require('qrcode-terminal').generate(loginUrl, {
+      small: true
+    })
   }
   console.log(`${url}\n[${code}] Scan QR Code in above url to login: `)
 })
 bot.on('login', user => console.log(`User ${user} logined`))
-bot.on('message',send)
+bot.on('message', send)
 bot.start()
 
-async function send(msg){
-  console.log(msg)
-  if(msg.payload.text==='发送'){
-    const helperContactA = await bot.Contact.find({ name: 'v.' })
-    await helperContactA.say('张玲亚真可爱啊~~~')
+async function send(msg) {
+  if (msg.self()) {
+    return
   }
-  
+  const contact = msg.from()
+  const text = msg.text()
+  const room = msg.room()
+  let json = {
+    "reqType": 0,
+    "perception": {
+      "inputText": {
+        "text": text
+      }
+    },
+    "userInfo": {
+      "apiKey": "d78796e6c234415ebb400df68516fecb",
+      "userId": msg.id
+    }
+  }
+
+  if (room) {
+    const title = await room.topic();
+    if (title === '哎呦，程序员了不起？') {
+      const results = await axios.post('http://openapi.tuling123.com/openapi/api/v2', JSON.stringify(json))
+      let data = results.data
+      let res = data.results
+      res = res[0]
+      room.say(res.values.text)
+    }
+  } else {
+    const results = await axios.post('http://openapi.tuling123.com/openapi/api/v2', JSON.stringify(json))
+    let data = results.data
+    let res = data.results
+    res = res[0]
+    console.log(res)
+    contact.say(res.values.text)
+  }
 }
